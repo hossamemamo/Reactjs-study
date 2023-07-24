4a1e5f25c57411826819fe819c3f3d9672c3bbf9
hossamemamo <78453559+hossamemamo@users.noreply.github.com>
2023-07-24 12:48:23 +0300

    section 15

--- Week10/comps/src/App.js
+++ Week10/comps/src/App.js
@@ -4,6 +4,7 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
 function App() {
   return (
     <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
@@ -25,6 +26,9 @@ function App() {
           <ModalPage/>
         </Route>
 
+        <Route path="/table">
+          <TablePage/>
+        </Route>
 
       </div>
     </div>
